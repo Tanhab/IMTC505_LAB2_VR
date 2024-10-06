276ee15c2c2523e1ca151cdce9faf44a58b73d3d
Tanhab Hossain <44816216+Tanhab@users.noreply.github.com>
2024-10-06 00:17:54 -0700

    Final cleanup

--- Assets/Scripts/RecipeMaker.cs
+++ Assets/Scripts/RecipeMaker.cs
@@ -18,13 +18,14 @@ public class RecipeMaker : MonoBehaviour
     public TMP_Text coffeeText;
     public TMP_Text sugarText;
     public TMP_Text modalText;
-    private float _sliderValue = 1.0f;
+    private float _waterAmount = 1.0f;
     private int _coffeeValue = 0;
     private int _sugarValue = 0;
     private bool _isMilkAdded;
     
     void Start()
     {
+        // For all the interactive UI element I added a listener when they are clicked o changed
         waterAmountSlider.onValueChanged.AddListener(OnSliderValueChanged);
         toggle.onValueChanged.AddListener(OnToggleValueChanged);
         increaseCoffeeButton.onClick.AddListener(IncreaseCoffee);
@@ -36,9 +37,9 @@ public class RecipeMaker : MonoBehaviour
 
     private void OnConfirm()
     {
-        if(_sliderValue < 1)
+        if(_waterAmount < 1)
             modalText.text = "No water no life, how dare you make coffee without water?\n";
-        else if(_sliderValue>1)
+        else if(_waterAmount>1)
             modalText.text = "Are you making juice or coffee!! too much water\n";
         else if(_coffeeValue <2)
             modalText.text = "Don't be cheapstake! pour more coffees!\n";
@@ -57,6 +58,7 @@ public class RecipeMaker : MonoBehaviour
                 "Well, I wouldn't say you are pablo piccasso of coffee!\nBut at least its better than starbucks!Congrats";
         }
         
+        // Deactivate this UI canvas and open the feedback canvas with text that I get from the above if conditions
         feedbackPanel.SetActive(true);
         gameObject.gameObject.SetActive(false);
     }
@@ -84,14 +86,16 @@ public class RecipeMaker : MonoBehaviour
         _coffeeValue = Math.Max(0, _coffeeValue - 1);
         coffeeText.text = $"Coffee\n{_coffeeValue} Spoon";
     }
+    // toggle for adding milk
     private void OnToggleValueChanged(bool arg0)
     {
         _isMilkAdded = arg0;
     }
-
+    
+    // When water slider is changed
     private void OnSliderValueChanged(float arg0)
     {
-        _sliderValue = arg0;
-        waterText.text = $"Use Sliders for amount of Water\nCurrent Amount - {_sliderValue} Cup";
+        _waterAmount = arg0;
+        waterText.text = $"Use Sliders for amount of Water\nCurrent Amount - {_waterAmount} Cup";
     }
 }
